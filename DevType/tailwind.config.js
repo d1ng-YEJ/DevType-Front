e248864e618553cc9559742037abb0a4f74f36c3
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-23 16:08:20 +0900

    다시 테일윈드 파일 설치 원래 꺼는 지우고

---
+++ DevType/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#0f172a",
+        white: "#f8fafc",
+        yellow: "#f59e0b",
+        perple: "#6366f1",
+        skyblue: "#22d3ee",
+        black: "#333d48",
+      },
+    },
+  },
+  plugins: [],
+};
