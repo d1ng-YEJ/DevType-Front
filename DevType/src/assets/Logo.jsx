00f8d4ece60a3fb25ebd801e00fe02b945963624
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-23 17:17:49 +0900

    feat:로고 컴포넌트 개발

---
+++ DevType/src/assets/Logo.jsx
@@ -0,0 +1,16 @@
+function Logo() {
+  return (
+    <div className="">
+      <div className="font-sans text-skyblue font-medium text-3xl mb-[32px]">
+        $analyze --github
+      </div>
+      <div className="flex">
+        <div className="font-sans text-white font-semibold text-6xl">
+          DevType
+        </div>
+        <div className="text-yellow font-medium text-6xl animate-blink">|</div>
+      </div>
+    </div>
+  );
+}
+export default Logo;
