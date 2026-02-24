eeb1f8bcedddf34f1edea289af8f36f2d6f516ea
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-24 22:36:55 +0900

    feat: 로고 개발

--- DevType/src/assets/Logo.jsx
+++ DevType/src/assets/Logo.jsx
@@ -1,11 +1,11 @@
 function Logo() {
   return (
     <div className="">
-      <div className="font-sans text-skyblue font-medium text-3xl mb-[32px]">
+      <div className="font-jet text-skyblue font-medium text-3xl mb-[32px]">
         $analyze --github
       </div>
       <div className="flex">
-        <div className="font-sans text-white font-semibold text-6xl">
+        <div className="font-jet text-white font-semibold text-6xl">
           DevType
         </div>
         <div className="text-yellow font-medium text-6xl animate-blink">|</div>
