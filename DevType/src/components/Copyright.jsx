6582a4d8c29fe2db601011e0363eacf6632dafff
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-24 22:37:12 +0900

    feat: 저작권 푸터 개발

---
+++ DevType/src/components/Copyright.jsx
@@ -0,0 +1,8 @@
+function Copyright() {
+  return (
+    <div className="text-white/50 font-medium text-[16px]">
+      &copy; 2026. d1ng&YEJ All rights reserved.
+    </div>
+  );
+}
+export default Copyright;
