b279e56d3345157e78b58c395dc03fde1683f5da
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-26 22:23:32 +0900

    fix: 라우터 경로 설정 및 시작 페이지를 잘못 개발한 거

--- DevType/src/App.jsx
+++ DevType/src/App.jsx
@@ -1,20 +1,12 @@
-import Logo from "./assets/Logo";
-import Copyright from "./components/Copyright";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Start from "./pages/Start";
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen mx-auto bg-navy">
-      <Logo></Logo>
-      <h2 className="text-white font-bold text-[24px] mt-[52px]">
-        개발자 전용 개발 성향 테스트
-      </h2>
-      <div className="text-white/50 font-light text-[16px] mt-[135px]">
-        Github URL을 입력하여 본인이 어떤 개발자인지 알아보세요!
-      </div>
-      <button className="text-black bg-yellow font-bold text-[20px] px-[165px] py-[12px] rounded-lg mt-[28px]">
-        시작하기
-      </button>
-      <Copyright></Copyright>
-    </div>
+    <Routes>
+      <Route path="/" element={<Start></Start>}></Route>
+      <Route path="/home" element={<Home></Home>}></Route>
+    </Routes>
   );
 }
 export default App;
