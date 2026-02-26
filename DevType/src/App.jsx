fd78df54d8d922cc0adfceeb4812196046f62833
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-26 18:59:32 +0900

    feat: 시작 페이지를 기본 URL로 설정 이거땜에 몇 분을 고생한거야 은준아 제발

--- DevType/src/App.jsx
+++ DevType/src/App.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Start from "./pages/Start";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Start></Start>}></Route>
       <Route path="/home" element={<Home></Home>}></Route>
     </Routes>
   );
