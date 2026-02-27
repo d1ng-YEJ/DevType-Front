35612064e3a861505cbb657cc809014112e76ca7
yangeunjun00 <s25027@gsm.hs.kr>
2026-02-28 00:07:48 +0900

    fix: 화면 너비 비율 안맞는거 수정

--- DevType/src/components/Top.jsx
+++ DevType/src/components/Top.jsx
@@ -1,30 +1,36 @@
 function Top() {
   return (
-    <div>
-      <div className="pt-4 pl-16">
-        <div className="font-jet text-skyblue font-medium text-[16px] mb-[12px]">
-          $analyze --github
-        </div>
-        <div className="flex gap-[1160px]">
-          <div className="flex">
-            <div className="font-jet text-white font-semibold text-[32px]">
-              DevType
-            </div>
-            <div className="text-yellow font-medium text-[32px] animate-blink">
-              |
-            </div>
+    <div className="w-full">
+      <div className="max-w-[1920px]  mx-auto">
+        <div className="pt-4 pl-16 pr-40">
+          <div className="font-jet text-skyblue font-medium text-[16px] mb-[12px]">
+            $analyze --github
           </div>
+          <div className="flex justify-between items-start ">
+            <div className="flex">
+              <div className="font-jet text-white font-semibold text-[32px]">
+                DevType
+              </div>
+              <div className="text-yellow font-medium text-[32px] animate-blink">
+                |
+              </div>
+            </div>
 
-          <div className="flex flex-col gap-2">
-            <div className="flex  gap-[68px]">
-              <div className="text-white/70 font-light text-5">홈</div>
-              <div className="text-white/70 font-light text-5">유형 소개</div>
+            <div className="flex flex-col gap-2">
+              <div className="flex  gap-[68px]">
+                <div className="text-white/70 font-light text-[16px]">홈</div>
+                <div className="text-white/70 font-light text-[16px]">
+                  유형 소개
+                </div>
+              </div>
+              <div className="relative right-8 bg-skyblue w-20 h-[2px] rounded"></div>
             </div>
-            <div className="relative right-8 bg-skyblue w-20 h-[2px] rounded"></div>
           </div>
+        </div>
+        <div className="px-8 mt-4">
+          <div className=" mx-auto bg-perple w-full h-[1px]"></div>
         </div>
       </div>
-      <div className="mt-4 mx-auto bg-perple w-[1560px] h-[1px]"></div>
     </div>
   );
 }
