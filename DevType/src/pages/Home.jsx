4dcb5e55e54421bb32f1377f2a801cc0d8d7b394
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-01 16:02:53 +0900

    feat: 메인페이지 퍼블리싱 야rrrr

--- DevType/src/pages/Home.jsx
+++ DevType/src/pages/Home.jsx
@@ -1,8 +1,39 @@
 import Top from "../components/Top";
+import Icon from "../assets/Vector.svg";
+import Copyright from "../components/Copyright";
 function Home() {
   return (
-    <div className="min-h-screen mx-0 my-0 bg-navy">
-      <Top></Top>
+    <div className="w-full h-full bg-navy">
+      <div className="max-w-[1920px] mx-auto ">
+        <Top></Top>
+        <div className="flex flex-col justify-center items-center h-80 mt-[160px]">
+          <div className="font-bold text-3xl text-white/80 pb-[72px]">
+            당신의 개발자 유형은?
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="font-jet text-skyblue">
+              &gt; Github 프로필 URL을 입력하세요
+            </div>
+            <div>
+              <input
+                type="url"
+                maxLength={30}
+                class="w-[524px] font-jet text-[16px] text-white/50 font-medium outline-none border-perple border-[1px] rounded-lg bg-[#142142] py-4 pl-5 pr-8 placeholder:font-jet text-[16px] font-medium focus:border-skyblue border-[1px] focus:shadow-[0_1px_10px_0px_#22d3ee]"
+                placeholder="https://github.com/username"
+              ></input>{" "}
+              <img
+                className="relative left-[470px] bottom-[37px]"
+                src={Icon}
+                alt="link icon"
+              ></img>
+            </div>
+          </div>
+          <button className="text-black bg-yellow font-bold text-[20px] px-[196px] py-3 rounded-lg mt-[28px] hover:bg-[#eb9400]">
+            AI 분석 시작하기
+          </button>
+          <Copyright></Copyright>
+        </div>
+      </div>
     </div>
   );
 }
