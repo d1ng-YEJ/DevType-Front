9ec87a08013eab0acff3fc4f9c4bcba6f7c05dab
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 08:37:40 +0900

    feat: type 경로 추가

--- DevType/src/App.jsx
+++ DevType/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Start from "./pages/Start";
+import Type from "./pages/Type";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Start></Start>}></Route>
       <Route path="/home" element={<Home></Home>}></Route>
+      <Route path="/type" element={<Type></Type>}></Route>
     </Routes>
   );
 }
