513e3f761af0355724f3d8e29ac039a678f84b1f
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 12:24:33 +0900

    delete: 불필요한 Top , Copyright 컴포넌트 삭제

---
+++ DevType/src/pages/Type.jsx
@@ -0,0 +1,8 @@
+function Type() {
+  return (
+    <div className="w-full h-full bg-navy">
+      <div className="flex flex-col justify-center items-center"></div>
+    </div>
+  );
+}
+export default Type;
