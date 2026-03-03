a6d97159c5990d542df7cc3e3879e091d158a762
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 17:53:57 +0900

    feat: 유형소개 컴포넌트 개발

---
+++ DevType/src/components/Types.jsx
@@ -0,0 +1,27 @@
+function Types({ developerType, explain, point1, point2, point3 }) {
+  return (
+    <div className="bg-[#142142] border-[1px] border-perple w-[236px] h-[360px] rounded-2xl mt-[120px]">
+      <div className="flex flex-col justify-center items-center gap-8 pt-8">
+        <div className="text-white text-2xl font-bold">{developerType}</div>
+        <div className="w-[200px] text-white/70 text-[12px] font-medium">
+          {explain}
+        </div>
+        <div className="flex flex-col  w-[200px]">
+          <div className="text-skyblue font-bold text-base pb-4">주요 특징</div>
+          <div className=" w-[200px] text-white font-medium text-[12px]">
+            {point1}
+          </div>
+          <br></br>
+          <div className=" w-[200px] text-white font-medium text-[12px]">
+            {point2}
+          </div>
+          <br></br>
+          <div className=" w-[200px] text-white font-medium text-[12px]">
+            {point3}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Types;
