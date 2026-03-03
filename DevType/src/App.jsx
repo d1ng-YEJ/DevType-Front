fff2609e24793d8239c735ab1345d825c799f357
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 12:25:36 +0900

    feat: Outlet으로 시작페이지를 제외한 페이지에 기본적으로 Top, Copyright 컴포넌트 설정

--- DevType/src/App.jsx
+++ DevType/src/App.jsx
@@ -1,13 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Start from "./pages/Start";
 import Type from "./pages/Type";
+import Top from "./components/Top";
+import Copyright from "./components/Copyright";
+function Layout() {
+  return (
+    <div className="flex flex-col bg-navy min-h-screen">
+      <Top></Top>
+      <main className="flex-1">
+        <Outlet></Outlet>
+      </main>
+      <Copyright></Copyright>
+    </div>
+  );
+}
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Start></Start>}></Route>
-      <Route path="/home" element={<Home></Home>}></Route>
-      <Route path="/type" element={<Type></Type>}></Route>
+
+      <Route element={<Layout></Layout>}>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/type" element={<Type></Type>}></Route>
+      </Route>
     </Routes>
   );
 }
