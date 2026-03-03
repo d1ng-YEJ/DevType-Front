b582cf561d8a0cbf3fd02a8a3b48c186fe8e7052
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 17:59:44 +0900

    feat: 유형 소개 컴포넌트 호버 테두리 변경 및 y축 이동 , 그림자 효과 추가

--- DevType/src/components/Types.jsx
+++ DevType/src/components/Types.jsx
@@ -1,6 +1,9 @@
 function Types({ developerType, explain, point1, point2, point3 }) {
   return (
-    <div className="bg-[#142142] border-[1px] border-perple w-[236px] h-[360px] rounded-2xl mt-[120px]">
+    <div
+      className="bg-[#142142] border-[1px] border-perple w-[236px] h-[360px] rounded-2xl mt-[120px] hover:border-skyblue transition-all duration-500 ease-in-out hover:translate-y-[-16px] hover:shadow-[0px_1px_10px_0px_#22d3ee]
+      "
+    >
       <div className="flex flex-col justify-center items-center gap-8 pt-8">
         <div className="text-white text-2xl font-bold">{developerType}</div>
         <div className="w-[200px] text-white/70 text-[12px] font-medium">
