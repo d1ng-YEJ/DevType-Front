eb5054fbaaffa967d0f5a7a21372ecd26d3e4fc5
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-03 18:27:14 +0900

    update: 최신 변경 내용 반영

--- DevType/src/components/Top.jsx
+++ DevType/src/components/Top.jsx
@@ -1,4 +1,25 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 function Top() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [moving, setMoving] = useState(location.pathname === "/type");
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    if (location.pathname === "/type") {
+      setMoving(true);
+    } else {
+      setMoving(false);
+      setAnimate(false);
+    }
+  }, [location.pathname]);
+  const handleClickType = () => {
+    if (location.pathname === "/type") return;
+    setAnimate(true);
+    setMoving(true);
+    navigate("/type");
+  };
+
   return (
     <div className="w-full">
       <div className="max-w-[1920px]  mx-auto">
@@ -18,12 +39,22 @@ function Top() {
 
             <div className="flex flex-col gap-2">
               <div className="flex  gap-[68px]">
-                <div className="text-white/70 font-light text-[16px]">홈</div>
-                <div className="text-white/70 font-light text-[16px]">
+                <div
+                  onClick={() => navigate("/home")}
+                  className="text-white/70 font-light text-[16px] hover:cursor-pointer"
+                >
+                  홈
+                </div>
+                <div
+                  onClick={handleClickType}
+                  className="text-white/70 font-light text-[16px] hover:cursor-pointer"
+                >
                   유형 소개
                 </div>
               </div>
-              <div className="relative right-8 bg-skyblue w-20 h-[2px] rounded"></div>
+              <div
+                className={`relative right-8 bg-skyblue w-20 h-[2px] rounded duration-500 ease-in-out transition-all ${moving ? "translate-x-[102px]" : "trnaslate-x-0"}`}
+              ></div>
             </div>
           </div>
         </div>
