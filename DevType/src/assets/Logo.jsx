804cbdb92384123a3ff875269c6eac1645d98eac
yangeunjun00 <s25027@gsm.hs.kr>
2026-03-25 16:07:08 +0900

    chage: 로고 DevType -> DPTI로 변경

--- DevType/src/assets/Logo.jsx
+++ DevType/src/assets/Logo.jsx
@@ -4,10 +4,8 @@ function Logo() {
       <div className="font-jet text-skyblue font-medium text-3xl mb-[32px]">
         $analyze --github
       </div>
-      <div className="flex">
-        <div className="font-jet text-white font-semibold text-6xl">
-          DevType
-        </div>
+      <div className="flex align-center justify-center">
+        <div className="font-jet text-white font-semibold text-6xl">DPTI</div>
         <div className="text-yellow font-medium text-6xl animate-blink">|</div>
       </div>
     </div>
